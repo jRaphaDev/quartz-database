1985f4145aa57db7f3c0b2fdc91c867197577cb6
raphael.santos <raphael.santos@DESA393.techne.com.br>
2016-09-06 17:16:53 -0300

    First commit quartz using database (SQL Server)

--- src/main/java/br/com/raphael/CronTriggerExample.java
+++ src/main/java/br/com/raphael/CronTriggerExample.java
@@ -1,27 +1,34 @@
 package br.com.raphael;
 
-import org.quartz.CronTrigger;
+import org.quartz.JobBuilder;
 import org.quartz.JobDetail;
 import org.quartz.Scheduler;
+import org.quartz.SimpleScheduleBuilder;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
 import org.quartz.impl.StdSchedulerFactory;
 
 public class CronTriggerExample {
 	
     public static void main( String[] args ) throws Exception {
 
-    	JobDetail job = new JobDetail();
-    	job.setName("jobName");
-    	job.setJobClass(HelloJob.class);
+    	JobDetail job = JobBuilder.newJob(HelloJob.class)
+    			.withIdentity("helloJob", "group1")
+    			.build();
 
-    	CronTrigger trigger = new CronTrigger();
-    	trigger.setName("triggerName");
-    	//aqui eu defino 10s para executar o job.
-    	trigger.setCronExpression("0/10 * * * * ?");
-
+    	Trigger trigger = TriggerBuilder
+            	.newTrigger()
+                .withIdentity("trigger1", "group1")
+                .startNow()
+                .withSchedule(
+                		SimpleScheduleBuilder.simpleSchedule()
+                        .withIntervalInSeconds(40).repeatForever())            
+                .build();
+    	
     	//Agendamento
     	Scheduler scheduler = new StdSchedulerFactory().getScheduler();
     	scheduler.start();
     	scheduler.scheduleJob(job, trigger);
-    
+    	    
     }
 }
--- src/main/java/br/com/raphael/HelloJob.java
+++ src/main/java/br/com/raphael/HelloJob.java
@@ -2,11 +2,14 @@ package br.com.raphael;
 
 import java.util.Date;
 
+
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
 public class HelloJob implements Job {
+	
+	//static Log logger = LogFactory.getLog(HelloJob.class); 
 	
 	@SuppressWarnings("deprecation")
 	public void execute(JobExecutionContext context) throws JobExecutionException {
